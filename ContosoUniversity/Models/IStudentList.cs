bc34b853d5b4450d708af8b1b634c094c1169bc1
Явор Налбантов <javor.nalbantov@gmail.com>
2026-05-14 23:39:11 +0300

    ok

---
+++ ContosoUniversity/Models/IStudentList.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class IStudentList
+    {
+
+
+    }
+
+    public class StudentItem
+    {
+        public int? ID { get; set; }
+
+
+        [Required]
+        public string? FName { get; set; }
+
+        [Required]
+        public string? LName { get; set; }
+
+        [Required]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public string PrintDate()
+        {
+            return this.EnrollmentDate.ToString("dd.MMM.yyyy");
+        }
+
+    }
+
+}
